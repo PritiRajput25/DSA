c4f41037667e2b278df9d41c48dc82c1969f3d49
PritiRajput25 <pritirajput25052005@gmail.com>
2026-08-13 09:22:35 +0530

    Add first-unique-character-in-a-string.java

---
+++ first-unique-character-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstUniqChar(String s) {
+     HashMap<Character,Integer> mp=new HashMap<>();
+     for(int i=0;i<s.length();i++){
+        char c=s.charAt(i);
+       mp.put(c,mp.getOrDefault(c,0)+1);
+     }
+     for(int i=0;i<s.length();i++){
+        if(mp.get(s.charAt(i))==1) return i;
+     }
+     return -1;
+    }
+}
