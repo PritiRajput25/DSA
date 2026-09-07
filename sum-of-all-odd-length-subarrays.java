da2eba4834b1f71400470070e04f7dfa47e44797
PritiRajput25 <pritirajput25052005@gmail.com>
2026-09-07 23:26:24 +0530

    Add sum-of-all-odd-length-subarrays.java

---
+++ sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int totalsum=0;
+    for(int i=0;i<arr.length;i++){
+        int sum=0;
+        for(int j=i;j<arr.length;j++){
+            sum+=arr[j];
+            if((j-i+1)%2!=0) totalsum+=sum;
+        }
+    }
+    return totalsum;    
+    }
+}
